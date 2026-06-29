ac883df347d5097b11e132bd0d1c5d5fe937acef
Tharunn D <83963600+tharunn2007@users.noreply.github.com>
2026-06-29 23:11:47 +0530

    Add files via upload

---
+++ L1.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+int main(){ // "main" is the standard way
+    printf("Hello\nWorld.\n "); //Newline \n
+    printf("Hello\tWorld"); //Tabspace \t
+    return 0; //always a good practice
+}
+/*Multi
+Line
+Comments*/
+
+/*
+STEP-1 Compiling: 
+
+Example file name:hello.c
+
+FORMAT  :   gcc hello.c -o sayhello
+
+gcc: c compiler accessed
+hello.c: file name
+-o sayhello: compiling under the executable file called sayhello
+*/
+
+/*
+STEP-2 Running:
+
+Example File name: hello.c
+
+Executable File name: sayhello
+
+FORMAT  :   ./sayhello  
+*/
